48afd6f99958283dddd94a87a6f62d57c11f9c27
bablooyerram <30732389+bablooyerram@users.noreply.github.com>
2017-08-06 15:17:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,12 @@ console.log('Loaded!');
 alert('babloo');
 var element = document.getElementById('babloo');
 element.innerHTML ='baa';
+var img = document.getElementById('bab');
+var mar =0;
+function mover(){
+    mar=mar+1;
+    img.stle.marginLeft=mar+'px';
+}
+img.onclick = function(){
+    var interval=setInterval(mover,50);
+};
